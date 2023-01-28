187b19f0cbfc4b83fb69e024eef1c0a7df5d7760
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2023-01-28 11:22:36 +0600

    Basic push

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
+//flutter section 2 Files
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+  void selectedAnswer(){
+    print("function called");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp (
@@ -12,7 +19,19 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title : Text("King"),
         ),
-        body: Text("King 2"),
+        body: Column(
+          children: [
+            Text("blah blah blaah, select an answer"),
+            ElevatedButton(
+              child: Text("1"),
+              onPressed: selectedAnswer, //function defined above
+            ),
+            ElevatedButton(
+              child: Text("2"),
+              onPressed: () => print("anonymous function"), // anonymous function
+            ),
+          ],
+        ),
       )
     );
   }
